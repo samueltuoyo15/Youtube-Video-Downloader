d87eee8fe564f5e5e049b62c8da0538026a72d01
Oritseweyinmi Samuel Tuoyo <samueltuoyo9082@gmail.com>
2024-10-11 13:58:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, 'dist')))
-app.use(cors())
+
 app.get('/download', async (req, res) => {
   let videoURL = req.query.url
 
