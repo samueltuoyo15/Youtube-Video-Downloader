d447785a93403beace9f3c09aa1a0c10adf32445
Oritseweyinmi Samuel Tuoyo <samueltuoyo9082@gmail.com>
2024-10-11 07:51:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import { fileURLToPath } from 'url'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
